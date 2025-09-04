458829a069638cd37ee1db19b63892630842d212
JJ00ng <whdgur0987@gmail.com>
2025-09-04 17:53:43 +0900

    Shop.h 작성

--- Sparta_Cpp_Assginment/Shop.h
+++ Sparta_Cpp_Assginment/Shop.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <vector>
+#include "Item.h"
+#include "Character.h"
+#include "Player.h"
+
 class Shop
 {
+private:
+	std::vector<Item*> items; //판매할 아이템 목록
+
 public:
 	Shop();
 	~Shop();
+
+	void ShowItems();                  //아이템 목록 출력
+	void BuyItem(Player& player);   //아이템 구매
+	void SellItem(Player& player);  //아이템 판매
 };
 
