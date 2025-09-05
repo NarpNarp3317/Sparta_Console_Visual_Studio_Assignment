c38f4224214094b53456d72b70a9b1ec5a57e06d
Jokimiddle <joki6180@gmail.com>
2025-09-05 11:24:23 +0900

    Item 하위 클래스 추가 및 consumable(소모품확인) 멤버변수 추가

--- Sparta_Cpp_Assginment/HealthPotion.h
+++ Sparta_Cpp_Assginment/HealthPotion.h
@@ -6,8 +6,11 @@ class HealthPotion : public Item
 private:
 	int amount;
 public:
-	HealthPotion(string name, int price, int amount)
-	: Item(name, price), amount(amount) {
+	HealthPotion(string name, int price, int amount, bool consumable = true, string description = "None")
+		: Item(name, price, consumable, description), amount(amount) {
+		if (amount < 1)
+			amount = 1;
+		if(description == "None")
 			this->setDescription("Restores "+to_string(amount)+" HP.");
 	}
 	~HealthPotion() {};
--- Sparta_Cpp_Assginment/Item.cpp
+++ Sparta_Cpp_Assginment/Item.cpp
@@ -6,21 +6,8 @@ void Item::use(Character* character)
 	// 아무 기능 없어도 함수 생성해두지 않으면 컴파일되지 않아 추가함
 }
 
-string Item::getName() {
-	return this->name;
-}
-
-int Item::getPrice()
-{
-	return this->price;
-}
-
-string Item::getDescription()
-{
-	return this->description;
-}
-
-void Item::setDescription(string desc)
-{
-	this->description = desc;
-}
+string Item::getName() { return this->name; }
+int Item::getPrice() { return this->price; }
+bool Item::isConsumable() { return this->consumable; }
+string Item::getDescription() { return this->description; }
+void Item::setDescription(string desc) { this->description = desc; }
