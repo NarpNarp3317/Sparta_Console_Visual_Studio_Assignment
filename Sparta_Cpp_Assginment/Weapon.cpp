c1f990897bdba6d369df5b0c62e1cb8907ca95ba
Jokimiddle <joki6180@gmail.com>
2025-09-08 15:40:31 +0900

    Weapon 클래스 use함수 수정
    use함수에서 객체가 깊은복사가 되지않아 오류를 발생시켰던 문제를 해결

--- Sparta_Cpp_Assginment/Weapon.cpp
+++ Sparta_Cpp_Assginment/Weapon.cpp
@@ -3,12 +3,13 @@
 void Weapon::use(Character* character)
 {
 	Weapon* curr_w = character->getEquippedWeapon();
-	character->addItem(curr_w);
+	if(curr_w != nullptr)
+		character->addItem(new Weapon(*curr_w));
+
 	curr_w = nullptr;
 	character->setEquippedWeapon(new Weapon(*this));
 	character->refreshATK();
 }
-
 
 int Weapon::getDamage() { return this->damage; }
 void Weapon::setDamage(int dmg) { this->damage = dmg; }
