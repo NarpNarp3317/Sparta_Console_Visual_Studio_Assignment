f37a8107bfec76b7cd8d42e61df4a0330dbf7c83
CranberryIsGoodTaste <104151171+CranberryMan@users.noreply.github.com>
2025-09-10 17:06:35 +0900

    * Scene Save and Load 가능하게 추가

--- Sparta_Cpp_Assginment/Scene.h
+++ Sparta_Cpp_Assginment/Scene.h
@@ -7,6 +7,6 @@
 struct Scene
 {
 	int _layer_priority;
-	std::vector < std::vector<T_Pixel>> _T_Pixel_frame;
+	std::vector<std::vector<T_Pixel>> _T_Pixel_frame;
 	std::vector<std::vector<bool>> _alpha;
 };
--- Sparta_Cpp_Assginment/Sparta_Cpp_Assginment.cpp
+++ Sparta_Cpp_Assginment/Sparta_Cpp_Assginment.cpp
@@ -19,12 +19,26 @@
 Shop shop;
 Character player("Test");
 
+#include "SceneSaveAndLoader.h";
+
 using namespace std;
+
+/// 씬 세이브 / 로더 예제
 
 int main()
 {
     ConsoleManager* C_manager = new ConsoleManager();
     GameManager_Layout* layout_GM = new GameManager_Layout(C_manager);
+    
+    // SCENE_TEST(); <<<<<<<<<<< 임시 테스트 라인입니다.
+    //{
+    //    SceneSaveAndLoader ssal;
+    //    ssal.saveScene(layout_GM->GetTexturePtr());
+
+    //    Scene tempScene = ssal.loadScene(SCENE_SAVE_FILENAME);
+    //    ssal.saveScene(&tempScene, "scenesave.save_1");
+    //}
+
     C_manager->Run_Update();// run the console manager
 
 	return 0;
