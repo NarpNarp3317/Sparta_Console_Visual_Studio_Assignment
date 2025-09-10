c5c60462ce03efa7ced529ba142e0c77d4061e3e
CranberryIsGoodTaste <104151171+CranberryMan@users.noreply.github.com>
2025-09-11 04:16:25 +0900

    * 불필요 코드 삭제

--- Sparta_Cpp_Assginment/SaveLoadManager.h
+++ Sparta_Cpp_Assginment/SaveLoadManager.h
@@ -24,39 +24,6 @@ public:
     bool LoadGame(Character* _player);
     bool saveDeleter();
 
-    void saveToNote(const std::string& filename, const std::string& content) {
-        // ofstream은 파일에 쓰기를 위한 클래스
-        std::ofstream file(filename);
-
-        if (file.is_open()) {
-            file << content; // 파일에 content를 씁니다.
-            file.close();    // 파일을 닫습니다.
-            std::cout << "메모장에 성공적으로 저장했습니다." << std::endl;
-        }
-        else {
-            std::cerr << "오류: 파일을 열 수 없습니다." << std::endl;
-        }
-    }
-
-    std::string loadFromNote(const std::string& filename) {
-        // ifstream은 파일에서 읽기를 위한 클래스
-        std::ifstream file(filename);
-        std::string content;
-        std::string line;
-
-        if (file.is_open()) {
-            while (std::getline(file, line)) { // 파일의 내용을 한 줄씩 읽습니다.
-                content += line + "\n";
-            }
-            file.close();
-            std::cout << "메모장에서 성공적으로 불러왔습니다." << std::endl;
-        }
-        else {
-            std::cerr << "오류: 파일을 열 수 없습니다." << std::endl;
-        }
-        return content;
-    }
-
 private:
     // const string WEAPON_DIVIDER = "@@@";
     // const string INVENTORY_DIVIDER = "###";
